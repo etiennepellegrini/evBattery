f407b5a6d03d0f0b4da08924b4c254a70095dbc6
Etienne Pellegrini (392M) <etienne.pellegrini@jpl.nasa.gov>
2023-02-08 18:37:20 -0800

    fix(sh): use keychain for authentication into Github

--- get_battery_stats.sh
+++ get_battery_stats.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR=$PWD
-EV_DIR=$(dirname ${BASH_SOURCE[0]})
+EV_DIR=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 cd $EV_DIR
 source .evBattery_env/bin/activate
@@ -9,12 +9,30 @@ source data/.credentials.sh
 cd src
 python get_battery_stats.py
 
-# Back data up on Github
+# --- Back data up
+# Local source control
 cd $EV_DIR
 git checkout data
 git add data/car_stats.jsonl
 git commit -m "update(data): push updated car stats"
+
+# Github push
+# Requires authentication into Github
+
+# Using keychain
+KEY="/usr/local/bin/keychain"
+if [[ -x $KEY ]]; then
+    eval $($KEY --eval --agents "ssh" $HOME/.ssh/github)
+
+# No keychain - use ssh-agent directly
+else
+    echo "keychain program not found. Might have GH authentication problems"
+
+fi
+
+# Push
 git push
 
+# --- Clean-up
 # Go back to initial dir
 cd $DIR
